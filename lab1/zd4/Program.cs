4af33913bc0c8137af1578c2166841965e5fe290
iobitt <sashabratm@gmail.com>
2022-02-19 15:51:40 +0300

    Задача №4

---
+++ lab1/zd4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Apis
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      int a;
+
+      bool success = int.TryParse(Console.ReadLine(), out a);
+      if (success)
+        Console.WriteLine(Math.Pow(a, 2));
+      else
+        Console.WriteLine("Некорректные входные данные!");
+
+      Console.ReadLine();
+    }
+  }
+}
