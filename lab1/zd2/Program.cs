446ea4e4e0655d1518468bd205e22b6bcafa131e
iobitt <sashabratm@gmail.com>
2022-02-19 15:20:08 +0300

    Задача №2

---
+++ lab1/zd2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Apis
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      int a = 5, b = 17;
+      int result = a * b;
+      Console.WriteLine(result);
+
+      Console.ReadLine();
+    }
+  }
+}
