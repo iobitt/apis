da4dd83fc92e05127a3756509faa4dc0cf6b845c
iobitt <sashabratm@gmail.com>
2022-02-19 14:57:56 +0300

    Задача №1

---
+++ lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Apis
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      int a = 5;
+      int b = 7 , c = 10;
+      int d = 82;
+      int e = 17;
+
+      int s = a + b + c + d + e;
+      Console.WriteLine("Сумма всех значений равна: {0}", s);
+
+      Console.ReadLine();
+    }
+  }
+}
